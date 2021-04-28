7a421d23ad2983cef15cddd4294f7c15e79d8117
aurelionsulll <mennanamehdi@gmail.com>
2021-04-28 08:10:19 +0000

    swiper bullets color black and white

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,22 +1,15 @@
-import Swiper from 'swiper';
+import { Swiper, Pagination } from 'swiper';
+Swiper.use(Pagination);
+
 const swiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	direction: 'vertical',
 	loop: true,
 
-	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination',
-	},
-
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		el: '.swiper-scrollbar',
+		clickable: true,
+		type: 'bullets',
+		left: '10px',
 	},
 });
